96a11b1cb59cc93ed7cf53a8c068fa47ab0b0fa7
Julien Picquet <julienpicquet1@gmail.com>
2022-06-15 11:17:43 +0200

    add incrementation on formateur

--- TP2/src/fr/diginamic/heritage2/Formateur.java
+++ TP2/src/fr/diginamic/heritage2/Formateur.java
@@ -5,12 +5,21 @@ import java.time.LocalDate;
 public class Formateur extends Individu {
     private int numeroIntervenant;
     private LocalDate dateDeCreation;
-    private static int nombreIntervenant;
+    private static int nombreIntervenant = 0;
 
-    public Formateur(String nom, String prenom, int telephone, int numeroIntervenant, LocalDate dateDeCreation, int nombreIntervenant) {
+    public Formateur(String nom, String prenom, int telephone, int numeroIntervenant, LocalDate dateDeCreation) {
         super(nom, prenom, telephone);
         this.numeroIntervenant = numeroIntervenant;
         this.dateDeCreation = dateDeCreation;
+        setNombreIntervenant(getNombreIntervenant() + 1);
+    }
+
+    public static int getNombreIntervenant() {
+        return nombreIntervenant;
+    }
+
+    public static void setNombreIntervenant(int nombreIntervenant) {
+        Formateur.nombreIntervenant = nombreIntervenant;
     }
 
     public int getNumeroIntervenant() {
